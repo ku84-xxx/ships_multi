fb16a2b240f7b437f443ae1de9e8ae0940a6c13e
ku84-xxx <kubwie@gmail.com>
2026-08-06 14:05:45 +0200

    Fix: </script> w minifikowanym JS zamykał przedwcześnie tag HTML

--- publish.js
+++ publish.js
@@ -59,7 +59,7 @@ async function main() {
     format: { comments: false },
   });
   if (result.error) throw result.error;
-  const js = result.code;
+  const js = result.code.replace(/<\/script/gi, "<\\/script");
 
   let out = src.replace(/<style>[\s\S]*?<\/style>/, "<style>" + css + "</style>");
   const scriptStart = out.indexOf(appScript[0]);
